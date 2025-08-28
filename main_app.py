5e33e479e880cf6c1f01b221ba8ce0a61b00ed3e
aoi <Aoi764658@gmail.com>
2025-08-29 01:24:20 +0900

    mainにコミット

--- main_app.py
+++ main_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title('mainアプリ')
-st.caption('これはテストアプリです。')
+st.caption('これはテストアプリです')
 
 image = Image.open('1546.png')
 st.image(image,width=200)
