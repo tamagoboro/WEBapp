78eeb3f7ce85dad4a172575f9bbef42c1934c648
aoi <Aoi764658@gmail.com>
2025-08-30 01:22:52 +0900

    得意情報確認アプリ制作

--- pages/得意情報確認.py
+++ pages/得意情報確認.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import json
 
-code = '''
-制作中です。
+# rはread(読む)のr
+json_openfile = open('specialty.json','r',encoding='utf-8')
 
-'''
-st.code(code)
+
+json_load = json.load(json_openfile)
+
+for name , info in json_load.values():
+
+    df =pd.DataFrame({
+        '名前':[name],
+        '所属':{info['Affiliation']},
+        'スキル':{info['skill']}
+    })
+    st.table(df.style.highlight_max(axis=0))
+    st.write(df)
