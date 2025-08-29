fdf880eaf896f83de36a037d596c2c61c24b7052
aoi <Aoi764658@gmail.com>
2025-08-30 01:28:11 +0900

    errorを編集

--- pages/得意情報確認.py
+++ pages/得意情報確認.py
@@ -9,12 +9,14 @@ json_openfile = open('specialty.json','r',encoding='utf-8')
 
 json_load = json.load(json_openfile)
 
-for name , info in json_load.values():
+data = []
 
-    df =pd.DataFrame({
-        '名前':[name],
-        '所属':{info['Affiliation']},
-        'スキル':{info['skill']}
+for name, info in json_load.items():
+    data.append({
+        '名前': name,
+        '所属': info['Affiliation'],
+        'スキル': info['skill']
     })
-    st.table(df.style.highlight_max(axis=0))
-    st.write(df)
+
+df = pd.DataFrame(data)
+st.table(df)
